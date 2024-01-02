17f61257ab1dd8f8920195d029f36d64d7e64338
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-01-02 16:40:30 +0100

    feat: add POST register method and tests

--- src/main/java/com/qxam/workmanagement/pages/controller/RegisterController.java
+++ src/main/java/com/qxam/workmanagement/pages/controller/RegisterController.java
@@ -1,19 +1,56 @@
 package com.qxam.workmanagement.pages.controller;
 
+import com.qxam.workmanagement.domain.User;
 import com.qxam.workmanagement.domain.dto.UserDto;
+import com.qxam.workmanagement.domain.exception.DuplicateDocuments;
+import com.qxam.workmanagement.mapper.UserMapper;
+import com.qxam.workmanagement.service.UserDbService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("register")
+@RequiredArgsConstructor
 public class RegisterController {
+
+  private final UserDbService service;
+  private final UserMapper mapper;
 
   @GetMapping
   public String registerPage(Model model) {
     UserDto userDto = new UserDto();
     model.addAttribute("user", userDto);
     return "register";
+  }
+
+  @PostMapping("/save")
+  public String registration(
+      @ModelAttribute("user") UserDto userDto,
+      BindingResult bindingResult,
+      Model model,
+      HttpServletResponse response) {
+    User user = mapper.mapToUser(userDto);
+
+    try {
+      service.saveUser(user);
+    } catch (DuplicateDocuments duplicateDocuments) {
+      bindingResult.rejectValue("email", StringUtils.EMPTY, "User already exists");
+    }
+
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("user", userDto);
+      response.setStatus(HttpServletResponse.SC_CONFLICT);
+      return "register";
+    }
+
+    return "index";
   }
 }
--- src/test/java/com/qxam/workmanagement/pages/controller/RegisterControllerTests.java
+++ src/test/java/com/qxam/workmanagement/pages/controller/RegisterControllerTests.java
@@ -1,12 +1,22 @@
 package com.qxam.workmanagement.pages.controller;
 
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.qxam.workmanagement.domain.User;
+import com.qxam.workmanagement.domain.dto.UserDto;
+import com.qxam.workmanagement.domain.exception.DuplicateDocuments;
+import com.qxam.workmanagement.mapper.UserMapper;
+import com.qxam.workmanagement.service.UserDbService;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,12 +25,56 @@ public class RegisterControllerTests {
 
   @Autowired private MockMvc mockMvc;
 
+  @MockBean private UserDbService service;
+
+  @MockBean private UserMapper mapper;
+
   @Test
   void shouldReturnRegisterView() throws Exception {
     this.mockMvc
         .perform(get("/register").accept(MediaType.APPLICATION_JSON))
         .andDo(print())
         .andExpect(status().isOk())
+        .andExpect(view().name("layouts/default-layout"))
+        .andExpect(model().attributeExists("view"))
+        .andExpect(model().attribute("view", "register"))
+        .andExpect(model().attributeExists("user"));
+  }
+
+  @Test
+  void shouldSaveUser() throws Exception {
+    // Given
+    UserDto userDto = new UserDto(new ObjectId(), "example@example.com", "12345");
+    User user = new User(new ObjectId(), "example@example.com", "12345");
+
+    when(mapper.mapToUser(userDto)).thenReturn(user);
+
+    // When + Then
+    this.mockMvc
+        .perform(
+            post("/register/save").flashAttr("user", userDto).accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("layouts/default-layout"))
+        .andExpect(model().attributeExists("view"))
+        .andExpect(model().attribute("view", "index"));
+  }
+
+  @Test
+  void shouldThrowUserAlreadyExists() throws Exception {
+    // Given
+    UserDto userDto = new UserDto(new ObjectId(), "example@example.com", "12345");
+    User user = new User(new ObjectId(), "example@example.com", "12345");
+
+    when(mapper.mapToUser(userDto)).thenReturn(user);
+    doThrow(DuplicateDocuments.class).when(service).saveUser(user);
+
+    // When + Then
+    this.mockMvc
+        .perform(
+            post("/register/save").flashAttr("user", userDto).accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isConflict())
         .andExpect(view().name("layouts/default-layout"))
         .andExpect(model().attributeExists("view"))
         .andExpect(model().attribute("view", "register"))
