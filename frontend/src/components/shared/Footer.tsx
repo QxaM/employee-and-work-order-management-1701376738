5fa5bcb108010e9769cf3632210ec445c9ff42ed
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-10-27 15:07:59 +0100

    feat - add Footer component

---
+++ frontend/src/components/shared/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center bg-qxam-secondary-lightest h-[2rem] sticky bottom-0 w-full">
+      <p>Piotr Gliszczyński &copy; 2024</p>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
 
 import MainNavigationHeader from '../components/shared/navigation/MainNavigationHeader.tsx';
+import Footer from '../components/shared/Footer.tsx';
 
 const RootPage = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <MainNavigationHeader />
-      <Outlet />
-    </>
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
