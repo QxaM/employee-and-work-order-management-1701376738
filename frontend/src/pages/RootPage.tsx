7ca5f06996b60a6078b37613a8e62936e03537d3
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-10-27 15:25:28 +0100

    feat - add window resize event and windowSize hook

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -2,11 +2,17 @@ import { Outlet } from 'react-router-dom';
 
 import MainNavigationHeader from '../components/shared/navigation/MainNavigationHeader.tsx';
 import Footer from '../components/shared/Footer.tsx';
+import useWindowSize from '../hooks/useWindowSize.tsx';
 
 const RootPage = () => {
+  const { width } = useWindowSize();
+
+  const isMobile = width < 768;
+
   return (
     <div className="flex flex-col min-h-screen">
-      <MainNavigationHeader />
+      {!isMobile && <MainNavigationHeader />}
+      {isMobile && <Footer />}
       <div className="flex-grow">
         <Outlet />
       </div>
