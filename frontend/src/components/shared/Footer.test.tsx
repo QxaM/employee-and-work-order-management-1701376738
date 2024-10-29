6f702eb93279629069601b89465cbc3c790554c7
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-10-29 20:46:15 +0100

    feat - add FooterTests

---
+++ frontend/src/components/shared/Footer.test.tsx
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Footer from './Footer.tsx';
+
+describe('Footer', () => {
+  it('Should contain author', () => {
+    // Given
+    const authorName = 'Piotr Gliszczyński';
+
+    // When
+    render(<Footer />, { wrapper: BrowserRouter });
+    const footerElement = screen.getByText(authorName, { exact: false });
+
+    // Then
+    expect(footerElement).toBeInTheDocument();
+  });
+});
