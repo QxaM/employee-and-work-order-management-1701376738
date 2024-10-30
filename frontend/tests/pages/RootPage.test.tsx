0262125873f4092fdff6ffb15a7b02d86d0f34bb
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-10-30 22:30:23 +0100

    feat - add RootPage Tests

---
+++ frontend/tests/pages/RootPage.test.tsx
@@ -0,0 +1,63 @@
+import { afterEach, beforeEach, describe, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import RootPage from '@/pages/RootPage.tsx';
+
+const mockWindowSize = vi.fn();
+vi.mock('@/hooks/useWindowSize.tsx', () => ({
+  default: () => mockWindowSize(),
+}));
+
+describe('Root Page', () => {
+  beforeEach(() => {
+    vi.resetModules();
+  });
+
+  afterEach(() => {
+    mockWindowSize.mockReset();
+  });
+
+  it('Should contain Footer', () => {
+    // Given
+    const authorName = 'Piotr Gliszczyński';
+    mockWindowSize.mockReturnValue({ width: 1024, height: 768 });
+
+    // When
+    render(<RootPage />, { wrapper: BrowserRouter });
+    const footerElement = screen.getByText(authorName, { exact: false });
+
+    // Then
+    expect(footerElement).toBeInTheDocument();
+  });
+
+  it('Should contain standard navigation header', () => {
+    // Given
+    const ariaLabel = 'Toggle navigation menu';
+    mockWindowSize.mockReturnValue({ width: 1024, height: 768 });
+
+    // When
+    render(<RootPage />, { wrapper: BrowserRouter });
+    const menuButton = screen.queryByRole('button', {
+      name: new RegExp(ariaLabel, 'i'),
+    });
+
+    // Then
+    expect(menuButton).not.toBeInTheDocument();
+  });
+
+  it('Should contain mobile navigation header', () => {
+    // Given
+    const ariaLabel = 'Toggle navigation menu';
+    mockWindowSize.mockReturnValue({ width: 500, height: 768 });
+
+    // When
+    render(<RootPage />, { wrapper: BrowserRouter });
+    const menuButton = screen.queryByRole('button', {
+      name: new RegExp(ariaLabel, 'i'),
+    });
+
+    // Then
+    expect(menuButton).toBeInTheDocument();
+  });
+});
