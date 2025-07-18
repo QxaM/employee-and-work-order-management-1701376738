dedb3ab91874d9c12b88574aac173bba0062d894
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-07-18 14:44:39 +0200

    feat - add addRole and removeRole api functions

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -51,6 +51,20 @@ export interface ModifyRoleRequest {
 const defaultRoleUpdateErrorMessage =
   'Error during role modification, try again later';
 
+/**
+ * Asynchronously adds a role to a specified user.
+ *
+ * This function sends a PATCH request to update the role assigned
+ * to a user. It constructs the request using the user's ID and the
+ * role's ID. The operation is abortable using an AbortSignal provided
+ * via the signal parameter.
+ *
+ * @param {ModifyRoleRequest} params - The parameters for modifying the role.
+ * @param {string} params.userId - The unique identifier of the user to whom the role will be added.
+ * @param {Object} params.role - The role object containing information about the role to add.
+ * @param {AbortSignal} params.signal - Signal object for request cancellation.
+ * @throws {Error} Throws an error if the fetch operation fails or if the response indicates a failure.
+ */
 export const addRole = async ({ userId, role, signal }: ModifyRoleRequest) => {
   const url = apiBaseUrl + USERS_API + `/${userId}/addRole?role=${role.id}`;
   await handleFetchVoid(
@@ -66,17 +80,35 @@ export const addRole = async ({ userId, role, signal }: ModifyRoleRequest) => {
   );
 };
 
+/**
+ * Removes a specific role from a user.
+ *
+ * This asynchronous function sends a PATCH request to the server to remove a designated
+ * role from the user specified by the `userId`. The request also includes an authorization
+ * token for authentication.
+ *
+ * @param {Object} params - The parameters for removing a role.
+ * @param {string} params.userId - The unique identifier of the user.
+ * @param {Object} params.role - The role object to be removed, containing at least an `id` property.
+ * @param {AbortSignal} [params.signal] - Optional AbortSignal to allow the request to be aborted.
+ *
+ * @throws {Error} Throws an error if the API call fails or the request is aborted.
+ */
 export const removeRole = async ({
   userId,
   role,
   signal,
 }: ModifyRoleRequest) => {
   const url = apiBaseUrl + USERS_API + `/${userId}/removeRole?role=${role.id}`;
-  await handleFetchVoid(url, {
-    method: 'PATCH',
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
+  await handleFetchVoid(
+    url,
+    {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      signal,
     },
-    signal,
-  });
+    defaultRoleUpdateErrorMessage
+  );
 };
--- frontend/tests/api/user.test.tsx
+++ frontend/tests/api/user.test.tsx
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, expect } from 'vitest';
-import { getUsers } from '../../src/api/user.ts';
+import { addRole, getUsers, removeRole } from '../../src/api/user.ts';
 import { ApiErrorType } from '../../src/types/ApiTypes.ts';
 import * as baseApiModule from '../../src/api/base.ts';
 
@@ -14,6 +14,17 @@ const USER_CONTENT = [
         name: 'OPERATOR',
       },
     ],
+  },
+];
+
+const ROLE_DATA = [
+  {
+    id: 14,
+    name: 'OPERATOR',
+  },
+  {
+    id: 15,
+    name: 'DESIGNER',
   },
 ];
 
@@ -81,6 +92,7 @@ const localStorageMock = {
 
 describe('User API', () => {
   const mockHandleFetch = vi.fn();
+  const mockHandleFetchVoid = vi.fn();
 
   beforeEach(() => {
     localStorageMock.getItem.mockClear();
@@ -88,6 +100,9 @@ describe('User API', () => {
 
     vi.stubGlobal('localStorage', localStorageMock);
     vi.spyOn(baseApiModule, 'handleFetch').mockImplementation(mockHandleFetch);
+    vi.spyOn(baseApiModule, 'handleFetchVoid').mockImplementation(
+      mockHandleFetchVoid
+    );
   });
 
   afterEach(() => {
@@ -159,6 +174,90 @@ describe('User API', () => {
 
       // When + Then
       await expect(getUsers({})).rejects.toThrow(apiMessage.message);
+    });
+  });
+
+  describe('addRole', () => {
+    it('Should patch add role', async () => {
+      // Given
+      mockHandleFetchVoid.mockResolvedValue(undefined);
+      localStorageMock.getItem.mockReturnValue('tokenValue');
+
+      const userId = USER_CONTENT[0].id;
+      const roles = ROLE_DATA.filter((role) =>
+        USER_CONTENT[0].roles.some((userRole) => userRole.id === role.id)
+      );
+
+      // When
+      await addRole({ userId, role: roles[0] });
+
+      // Then
+      expect(mockHandleFetchVoid).toHaveBeenCalledWith(
+        expect.stringContaining(`/users/${userId}/addRole?role=${roles[0].id}`),
+        expect.objectContaining({
+          method: 'PATCH',
+          headers: {
+            Authorization: 'Bearer tokenValue',
+          },
+        }),
+        expect.any(String)
+      );
+    });
+
+    it('Should throw if error during adding a role', async () => {
+      // Given
+      const error = 'Forbidden';
+      mockHandleFetchVoid.mockRejectedValue(new Error(error));
+      localStorageMock.getItem.mockReturnValue('invalid');
+
+      const userId = USER_CONTENT[0].id;
+      const roles = ROLE_DATA.filter((role) =>
+        USER_CONTENT[0].roles.some((userRole) => userRole.id === role.id)
+      );
+
+      // When + Then
+      await expect(addRole({ userId, role: roles[0] })).rejects.toThrowError(
+        error
+      );
+    });
+  });
+
+  describe('removeRole', () => {
+    it('Should patch remove role', async () => {
+      // Given
+      mockHandleFetchVoid.mockResolvedValue(undefined);
+      localStorageMock.getItem.mockReturnValue('tokenValue');
+
+      const userId = USER_CONTENT[0].id;
+      const role = USER_CONTENT[0].roles[0];
+
+      // When
+      await removeRole({ userId, role });
+
+      // Then
+      expect(mockHandleFetchVoid).toHaveBeenCalledWith(
+        expect.stringContaining(`/users/${userId}/removeRole?role=${role.id}`),
+        expect.objectContaining({
+          method: 'PATCH',
+          headers: {
+            Authorization: 'Bearer tokenValue',
+          },
+        }),
+        expect.any(String)
+      );
+    });
+
+    it('Should throw if error during removing a role', async () => {
+      // Given
+      const error = 'Forbidden';
+      mockHandleFetchVoid.mockRejectedValue(new Error(error));
+      localStorageMock.getItem.mockReturnValue('invalid');
+
+      const userId = USER_CONTENT[0].id;
+      const role = USER_CONTENT[0].roles[0];
+
+      // When + Then
+      await expect(removeRole({ userId, role })).rejects.toThrowError(error);
     });
   });
 });
