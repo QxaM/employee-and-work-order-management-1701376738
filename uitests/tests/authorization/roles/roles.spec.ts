d7c24af50784a1c34de1cbbba8f545c57c32f0f6
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-07-30 16:51:54 +0200

    feat - add TC-19 renavigate home after non-authorized page access

--- uitests/tests/authorization/roles/roles.spec.ts
+++ uitests/tests/authorization/roles/roles.spec.ts
@@ -1,6 +1,11 @@
 import { expect, test } from "../../base/baseTest";
 import { openAdminPage } from "../../utils/navigation.utils";
-import { notLoggedInMessage } from "./roles.utils";
+import { notAuthorizedMessage, notLoggedInMessage } from "./roles.utils";
+import { buildContextStorage, ContextState } from "../../setup/setup.utils";
+
+import credentials from "../../../test-data/credentials.json";
+import { loginApi } from "../../utils/api.utils";
+import { Page } from "playwright";
 
 test("TC18 - should navigate to login if user is not logged in", async ({
   page,
@@ -13,4 +18,38 @@ test("TC18 - should navigate to login if user is not logged in", async ({
   // Then
   await expect(page).toHaveURL("/login");
   await expect(notLoggedInMessage(page)).toBeVisible();
+});
+
+test("TC19 - should navigate to home if user is not authorized", async ({
+  browser,
+  apiContext,
+  baseURL,
+}) => {
+  let contextState: ContextState;
+
+  await test.step("TC-19.1 - create login state", async () => {
+    // Given
+    const userCredentials = credentials.operator;
+
+    // When
+    const token = await loginApi(apiContext, userCredentials);
+
+    // Then
+    expect(token).toBeDefined();
+    expect(token.token).toBeDefined();
+    contextState = buildContextStorage(baseURL, token);
+  });
+
+  let page: Page;
+  await test.step("TC-19.2 - navigate to not authorized page", async () => {
+    // Given
+    page = await browser.newPage({ storageState: contextState });
+
+    // When
+    await openAdminPage(page);
+
+    // Then
+    await expect(page).toHaveURL("/");
+    await expect(notAuthorizedMessage(page)).toBeVisible();
+  });
 });
--- uitests/tests/authorization/roles/roles.utils.ts
+++ uitests/tests/authorization/roles/roles.utils.ts
@@ -3,3 +3,9 @@ import { Page } from "playwright";
 export const notLoggedInMessage = (page: Page) => {
   return page.getByText("You are not logged in! Please log in.");
 };
+
+export const notAuthorizedMessage = (page: Page) => {
+  return page.getByText(
+    "You are not authorized to access this page. Please log in.",
+  );
+};
