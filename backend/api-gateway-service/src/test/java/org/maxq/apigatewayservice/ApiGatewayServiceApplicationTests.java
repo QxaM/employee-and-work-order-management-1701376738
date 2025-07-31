5e6a7346eba7213fde67dd61279f307d1b082c55
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-07-31 21:57:38 +0200

    feat - add api-gateway module

---
+++ backend/api-gateway-service/src/test/java/org/maxq/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maxq.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
