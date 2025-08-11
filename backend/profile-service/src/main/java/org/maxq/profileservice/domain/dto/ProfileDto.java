dc39737574daa814e90dc74152d4b3a3064b9267
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-08-11 14:38:27 +0200

    feat - add ProfileDto

---
+++ backend/profile-service/src/main/java/org/maxq/profileservice/domain/dto/ProfileDto.java
@@ -0,0 +1,17 @@
+package org.maxq.profileservice.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProfileDto {
+
+  private Long id;
+  private String email;
+  private String firstName;
+  private String middleName;
+  private String lastName;
+}
