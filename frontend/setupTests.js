8645f802607435bf439b88a8c0e307cc5f2cbf63
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-08-26 22:45:32 +0200

    fix - delete unused packages

--- frontend/setupTests.js
+++ frontend/setupTests.js
@@ -3,10 +3,8 @@ import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/vitest';
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import { MotionGlobalConfig } from 'framer-motion';
 
 globalThis.React = React;
-MotionGlobalConfig.skipAnimations = true;
 
 class ResizeObserver {
   cb;
