6aebb0ff792a7014adafa8ae0aeed953b764ee07
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-08-31 13:48:14 +0200

    test - add TC23 to test if profile was created and displayed correctly

--- uitests/tests/utils/navigation.utils.ts
+++ uitests/tests/utils/navigation.utils.ts
@@ -16,6 +16,10 @@ export const openUpdatePasswordPage = async (page: Page, token: string) => {
   await page.goto(`/password/confirm?token=${token}`);
 };
 
+export const openProfilePage = async (page: Page) => {
+  await page.goto("/profile");
+};
+
 export const openAdminPage = async (page: Page) => {
   await page.goto("/admin");
 };
