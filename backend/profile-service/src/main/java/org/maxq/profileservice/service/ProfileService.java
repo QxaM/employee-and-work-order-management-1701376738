977d7c07f6d7d3beb1aeab17a0fa19e5d06e8063
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-09-01 21:31:46 +0200

    feat - add update options in ProfileService

--- backend/profile-service/src/main/java/org/maxq/profileservice/service/ProfileService.java
+++ backend/profile-service/src/main/java/org/maxq/profileservice/service/ProfileService.java
@@ -1,6 +1,7 @@
 package org.maxq.profileservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.maxq.profileservice.domain.Profile;
 import org.maxq.profileservice.domain.exception.DataValidationException;
 import org.maxq.profileservice.domain.exception.DuplicateEmailException;
@@ -10,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionSystemException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
@@ -31,6 +33,23 @@ public class ProfileService {
     } catch (DataIntegrityViolationException e) {
       throw new DuplicateEmailException(
           "User with this email already exists! Email: " + profile.getEmail(), e);
+    }
+  }
+
+  public void updateProfile(Profile profile) throws DataValidationException {
+    Profile profileToSave;
+    try {
+      Profile foundProfile = this.getProfileByEmail(profile.getEmail());
+      profileToSave = new Profile(foundProfile.getId(), profile.getEmail(), profile.getFirstName(), profile.getMiddleName(), profile.getLastName());
+    } catch (ElementNotFoundException e) {
+      log.warn("Profile not found when updating, creating new profile: {}", profile.getEmail(), e);
+      profileToSave = new Profile(profile.getEmail(), profile.getFirstName(), profile.getMiddleName(), profile.getLastName());
+    }
+
+    try {
+      profileRepository.save(profileToSave);
+    } catch (TransactionSystemException e) {
+      throw new DataValidationException("Failed email or password validation", e);
     }
   }
 
