3e51059907959c42faaaf0d0ba6ad9f8c0f30ad4
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-09-02 16:50:28 +0200

    test - add profile update test

--- uitests/tests/profile/profiles.spec.ts
+++ uitests/tests/profile/profiles.spec.ts
@@ -1,7 +1,11 @@
 import { expect, test } from "../base/baseTest";
 import { Credentials, Token } from "../types/Authorization";
 import { openHomePage, openProfilePage } from "../utils/navigation.utils";
-import { navigateToProfile } from "./profiles.utils";
+import {
+  clickEditProfile,
+  fillProfileDetails,
+  navigateToProfile,
+} from "./profiles.utils";
 
 import credentials from "../../test-data/credentials.json";
 import profiles from "../../test-data/profiles.json";
@@ -9,6 +13,7 @@ import { ProfileData } from "../types/Profile";
 import { getMyProfile } from "../utils/profile.api.utils";
 import { loginApi } from "../utils/authorization.api.utils";
 import { buildContextStorage } from "../setup/setup.utils";
+import { faker } from "@faker-js/faker";
 
 test("TC22 - should navigate and display user profile", async ({
   adminPage,
@@ -90,5 +95,65 @@ test("TC23 - should show newly registered profile", async ({
         page.getByRole("link", { name: registeredUser.email }),
       ).toBeVisible(),
     ]);
+  });
+});
+
+test("TC24 - should update profile", async ({
+  apiContext,
+  registeredUser,
+  baseURL,
+  browser,
+}) => {
+  await test.step("TC24.1 - profile should already exits", async () => {
+    await expect(async () => {
+      // Given
+      const email = registeredUser.email;
+      const roles = ["ROLE_OPERATOR"];
+
+      // When
+      let profileData: ProfileData = await getMyProfile(
+        apiContext,
+        email,
+        roles,
+      );
+
+      // Then
+      expect(profileData).toBeDefined();
+    }).toPass();
+  });
+
+  await test.step("TC24.2 - profile should be updated", async () => {
+    // Given
+    const firstName = faker.person.firstName();
+    const middleName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+
+    const email = registeredUser.email;
+    const password = registeredUser.password;
+    const token: Token = await loginApi(apiContext, { login: email, password });
+    const context = buildContextStorage(baseURL, token);
+    const page = await browser.newPage({ storageState: context });
+
+    await openProfilePage(page);
+
+    // When
+    await clickEditProfile(page);
+    await fillProfileDetails(page, {
+      firstName,
+      middleName,
+      lastName,
+    });
+    await page.getByRole("button", { name: "Save" }).click();
+
+    // Then
+    await expect(async () => {
+      await Promise.all([
+        await expect(
+          page.locator("section").getByText(firstName, { exact: true }),
+        ).toBeVisible(),
+        await expect(page.getByText(middleName, { exact: true })).toBeVisible(),
+        await expect(page.getByText(lastName, { exact: true })).toBeVisible(),
+      ]);
+    }).toPass();
   });
 });
--- uitests/tests/profile/profiles.utils.ts
+++ uitests/tests/profile/profiles.utils.ts
@@ -1,7 +1,34 @@
 import { Page } from "playwright";
 
+interface UpdateProfileType {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+}
+
 export const navigateToProfile = async (page: Page, email: string) => {
   const avatarLetter = email.charAt(0).toUpperCase();
   await page.getByRole("button", { name: avatarLetter, exact: true }).click();
   await page.getByRole("menuitem", { name: "Profile" }).click();
+};
+
+export const clickEditProfile = async (page: Page) => {
+  await page.getByRole("button", { name: "Edit profile" }).click();
+};
+
+export const clickSaveProfile = async (page: Page) => {
+  await page.getByRole("button", { name: "Save changes" }).click();
+};
+
+export const fillProfileDetails = async (
+  page: Page,
+  profile: UpdateProfileType,
+) => {
+  await page
+    .getByRole("textbox", { name: "first name" })
+    .fill(profile.firstName);
+  await page
+    .getByRole("textbox", { name: "middle name" })
+    .fill(profile.middleName ?? "");
+  await page.getByRole("textbox", { name: "last name" }).fill(profile.lastName);
 };
