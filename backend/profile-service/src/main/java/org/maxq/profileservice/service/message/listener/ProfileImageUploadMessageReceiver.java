7569c9af6b3e05c8818c0c16371dfe3c7937254f
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-09-14 14:16:14 +0200

    feat - add ProfileImageUpload message consumer

---
+++ backend/profile-service/src/main/java/org/maxq/profileservice/service/message/listener/ProfileImageUploadMessageReceiver.java
@@ -0,0 +1,27 @@
+package org.maxq.profileservice.service.message.listener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.maxq.profileservice.domain.dto.ImageDto;
+import org.maxq.profileservice.service.message.handler.ProfileImageUploadHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ProfileImageUploadMessageReceiver implements MessageReceiver<ImageDto> {
+
+  private final ProfileImageUploadHandler handler;
+
+  @RabbitListener(id = "profileImageUploadReceiver", queues = "${profile.queue.image.upload}")
+  public void receiveUploadMessage(ImageDto image) {
+    receiveMessage(image);
+  }
+
+  @Override
+  public void receiveMessage(ImageDto image) {
+    log.info("Received message: {}", image);
+    handler.handleMessage(image);
+  }
+}
