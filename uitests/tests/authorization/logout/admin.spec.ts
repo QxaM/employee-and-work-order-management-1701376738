d97ba24705babaf0387be953b9f2ebd4db10fae1
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-10-12 21:14:46 +0200

    fix - change failing tests after profile card on main navigation changed

--- uitests/tests/authorization/logout/admin.spec.ts
+++ uitests/tests/authorization/logout/admin.spec.ts
@@ -1,20 +1,14 @@
 import { expect, test } from "../../base/baseTest";
 import { openHomePage } from "../../utils/navigation.utils";
-import credentials from "../../../test-data/credentials.json";
-import { Credentials } from "../../types/Authorization";
 
 test("TC21 - role elements should be hidden after logout", async ({
   adminPage,
 }) => {
   // Given
-  const userCredentials = credentials.admin as Credentials;
-  const avatarLetter = userCredentials.login.charAt(0).toUpperCase();
   await openHomePage(adminPage);
 
   // When
-  await adminPage
-    .getByRole("button", { name: avatarLetter, exact: true })
-    .click();
+  await adminPage.getByRole("button").locator("img").click();
   await adminPage.getByText("Logout").click();
 
   // Then
