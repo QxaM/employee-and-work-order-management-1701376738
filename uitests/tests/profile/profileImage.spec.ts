2bdcc3d6c3b4431411bd8f970451e37c28ef86cc
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-10-16 15:49:25 +0200

    feat - add TC28 test implmementation

--- uitests/tests/profile/profileImage.spec.ts
+++ uitests/tests/profile/profileImage.spec.ts
@@ -6,7 +6,7 @@ import {getMyProfile} from "../utils/profile.api.utils";
 import {Token} from "../types/Authorization";
 import {buildContextStorage} from "../setup/setup.utils";
 import {Page} from "playwright";
-import {clickEditProfile, clickUploadImage} from "./profiles.utils";
+import {clickEditProfile, clickSaveProfile, clickUploadImage,} from "./profiles.utils";
 
 test("TC26 - should show profile image", async ({ adminPage }) => {
   // Given
@@ -77,6 +77,67 @@ test("TC27 - should validate profile image during upload", async ({
       page.getByText(
         "Invalid file name. Only numbers and characters are allowed",
       ),
+    ).toBeVisible();
+  });
+});
+
+test("TC28 - should show success when success", async ({
+  baseURL,
+  browser,
+  apiContext,
+  registeredUser,
+}) => {
+  await test.step("TC28.1 - profile should already exits", async () => {
+    await expect(async () => {
+      // Given
+      const email = registeredUser.email;
+      const roles = ["ROLE_OPERATOR"];
+
+      // When
+      let profileData: ProfileData = await getMyProfile(
+        apiContext,
+        email,
+        roles,
+      );
+
+      // Then
+      expect(profileData).toBeDefined();
+    }).toPass();
+  });
+
+  let page: Page;
+
+  await test.step("TC28.2 - login user", async () => {
+    // Given
+    const email = registeredUser.email;
+    const password = registeredUser.password;
+
+    // When
+    const token: Token = await loginApi(apiContext, { login: email, password });
+    const context = buildContextStorage(baseURL, token);
+    page = await browser.newPage({ storageState: context });
+
+    // Then
+    expect(page).toBeDefined();
+  });
+
+  await test.step("TC28.3 - should upload profile image", async () => {
+    // Given
+    await openProfilePage(page);
+    await clickEditProfile(page);
+
+    // When
+    const fileChooserPromise = page.waitForEvent("filechooser");
+    await clickUploadImage(page);
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles("resources/images/correctJpeg.jpg");
+    await clickSaveProfile(page);
+
+    // Then
+    await expect(
+      page
+        .getByTestId("modal-message")
+        .getByText("Profile image updated successfully"),
     ).toBeVisible();
   });
 });
