fb10be4a2627d712ed363ecbf33b041bcbad39d6
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-10-28 21:56:32 +0100

    feat - add TC31, TC32 implementations - redirection and back button blocking after logout

--- uitests/tests/authorization/logout/logout.spec.ts
+++ uitests/tests/authorization/logout/logout.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "../../base/baseTest";
-import { openHomePage } from "../../utils/navigation.utils";
+import { openAdminPage, openHomePage } from "../../utils/navigation.utils";
 import { clickLogout, logoutSuccessfulMessage } from "./logout.utils";
+import { notAuthorizedMessage, notLoggedInMessage } from "../roles/roles.utils";
 
 test("TC30 - should logout correctly", async ({ adminPage, baseURL }) => {
   // Given
@@ -14,4 +15,40 @@ test("TC30 - should logout correctly", async ({ adminPage, baseURL }) => {
     expect(logoutSuccessfulMessage(adminPage)).toBeVisible(),
     expect(adminPage.url()).toEqual(baseURL + "/"),
   ]);
+});
+
+test("TC31 - should correctly redirect from protected route", async ({
+  adminPage,
+  baseURL,
+}) => {
+  // Given
+  await openAdminPage(adminPage);
+
+  // When
+  await clickLogout(adminPage);
+
+  // Then
+  await Promise.all([
+    expect(notAuthorizedMessage(adminPage)).toBeHidden(),
+    expect(logoutSuccessfulMessage(adminPage)).toBeVisible(),
+    expect(adminPage.url()).toEqual(baseURL + "/"),
+  ]);
+});
+
+test("TC32 - should block back button to protected route", async ({
+  adminPage,
+  baseURL,
+}) => {
+  // Given
+  await openAdminPage(adminPage);
+  await clickLogout(adminPage);
+
+  await logoutSuccessfulMessage(adminPage).waitFor({ state: "visible" });
+
+  // When
+  await adminPage.goBack();
+
+  // Then
+  await expect(notLoggedInMessage(adminPage).first()).toBeVisible();
+  expect(adminPage.url()).toEqual(baseURL + "/login");
 });
