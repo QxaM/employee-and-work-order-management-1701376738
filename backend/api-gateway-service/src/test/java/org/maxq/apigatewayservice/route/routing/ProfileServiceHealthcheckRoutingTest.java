693923bd841007612534021568c442035651ef6a
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-10-31 10:49:15 +0100

    feat - add Routing directly to healthcheck APIs through Gateway

---
+++ backend/api-gateway-service/src/test/java/org/maxq/apigatewayservice/route/routing/ProfileServiceHealthcheckRoutingTest.java
@@ -0,0 +1,86 @@
+package org.maxq.apigatewayservice.route.routing;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Duration;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@WireMockTest(httpPort = 8082)
+@TestPropertySource(
+    properties = {
+        "eureka.client.enabled=false"
+    }
+)
+class ProfileServiceHealthcheckRoutingTest {
+
+  @LocalServerPort
+  private int port;
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @BeforeEach
+  void setUp() {
+    String baseUrl = "http://localhost:" + port;
+    this.webTestClient =
+        WebTestClient.bindToServer()
+            .responseTimeout(Duration.ofSeconds(10))
+            .baseUrl(baseUrl)
+            .build();
+
+    stubFor(WireMock.get("/actuator/health")
+        .willReturn(WireMock.ok()));
+  }
+
+  @Test
+  void shouldCallDownstreamService() {
+    // Given
+    String authUrl = "/api/profile";
+
+    // When
+    webTestClient.get()
+        .uri(authUrl + "/actuator/health")
+        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .exchange()
+        .expectStatus().isOk();
+
+    // Then
+    verify(1, WireMock.getRequestedFor(WireMock.urlEqualTo("/actuator/health")));
+  }
+
+  @Test
+  void shouldRewriteContentType() {
+    // Given
+    String authUrl = "/api/profile";
+
+    // When
+    webTestClient.get()
+        .uri(authUrl + "/actuator/health")
+        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML_VALUE)
+        .exchange()
+        .expectStatus().isOk();
+
+    // Then
+    verify(1,
+        WireMock.getRequestedFor(WireMock.urlEqualTo("/actuator/health"))
+            .withHeader(
+                HttpHeaders.ACCEPT,
+                WireMock.equalTo(MediaType.APPLICATION_JSON_VALUE)
+            ));
+  }
+}
