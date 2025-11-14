9ed058cea845b21dd4e2c64f950e84050139a34a
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2025-11-14 10:07:18 +0100

    feat - add Entities to User and Role

--- backend/task-service/src/main/java/org/maxq/taskservice/domain/Role.java
+++ backend/task-service/src/main/java/org/maxq/taskservice/domain/Role.java
@@ -1,18 +1,28 @@
 package org.maxq.taskservice.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Entity
+@Table(
+    name = "ROLES"
+)
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Role {
 
+  @Id
   private Long id;
+
+  @Column(unique = true)
   private String name;
+
+  @ManyToMany(mappedBy = "roles")
   private List<User> users;
 
 }
--- backend/task-service/src/main/java/org/maxq/taskservice/domain/User.java
+++ backend/task-service/src/main/java/org/maxq/taskservice/domain/User.java
@@ -1,18 +1,33 @@
 package org.maxq.taskservice.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@Entity
+@Table(
+    name = "USERS"
+)
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
+  @Id
   private Long id;
+
+  @Column(unique = true, nullable = false)
   private String email;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name = "USER_ROLE",
+      joinColumns = @JoinColumn(name = "USER_ID"),
+      inverseJoinColumns = @JoinColumn(name = "ROLE_ID")
+  )
   private Set<Role> roles;
 
 }
